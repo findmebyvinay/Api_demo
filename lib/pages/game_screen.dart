8e60ff2ebfebefe3db8dd6fdd53926e0bff35644
vinay <vinaythapa762@gmail.com>
2024-08-22 23:38:34 +0545

    just practice

--- lib/pages/game_screen.dart
+++ lib/pages/game_screen.dart
@@ -22,73 +22,93 @@ class _GameScreenState extends State<GameScreen> {
       ),),
       centerTitle: true,
     ),
-    body:Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Lottie.asset('lib/assets/xo.json',
-          height: 100,
-          width: 200),
-          Center(
-            child: Text('Lets Start the Game🕹️',
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold
-            ),),
-          ),
-          const SizedBox(height:0,),
-          Consumer<GameProvider>(
-              builder: (context, gameProvider, child) {
-                return Text(
-                Provider.of<GameProvider>(context).winner != ''
-                      ? 'Winner: ${ Provider.of<GameProvider>(context).winner}'
-                      : 'Current Player: ${ Provider.of<GameProvider>(context).currentplayer}',
-                  style: const TextStyle(fontSize: 24),
-                );
-              },
-            ),
-            const SizedBox(height: 10,),
-          Expanded(
-            child: Container(
-              height: 200,
-              width: 400,
-              child: GridView.builder(
-                shrinkWrap: true,
-                physics: const NeverScrollableScrollPhysics(),
-                gridDelegate:const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 3),
-                itemCount: 9,
-                 itemBuilder:(context,index){
-                   return AnimatedBuilder(animation:Provider.of<GameProvider>(context),
-                    builder:(context,child){
-                  return GestureDetector(
-                    onTap: () =>Provider.of<GameProvider>(context, listen: false).makeMove(index),
-                    child: Container(
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Colors.black),
-                      ),
-                      child: Center(
-                        child: Text(
-                         Provider.of<GameProvider>(context).board[index],
-                          style: const TextStyle(fontSize: 40),
+    body:Consumer<GameProvider>(
+      builder: (context, value, child) {
+        final gameProvider = Provider.of<GameProvider>(context);
+      return Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+          
+                
+                //   if (gameProvider.winner != '') {
+                //     return Text(
+                //       'Winner: ${ Provider.of<GameProvider>(context).winner}',
+                //       style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                //     );
+                //   } else {
+                //     return Text(
+                //       'Current Player: ${gameProvider.currentplayer}',
+                //       style: const TextStyle(fontSize: 24),
+                //     );
+                //   }
+                // },
+            
+            Lottie.asset('lib/assets/xo.json',
+            height: 100,
+            width: 200),
+            // const Center(
+            //   child: Text('Lets Start the Game🕹️',
+            //   style: TextStyle(
+            //     fontSize: 20,
+            //     fontWeight: FontWeight.bold
+            //   ),),
+            // ),
+              Text(
+                  gameProvider.gameOver
+                        ? 'Winner: ${gameProvider.winner}'
+                        : 'Current Player: ${ gameProvider.currentplayer}',
+                    style: const TextStyle(fontSize: 24),
+                  ),
+            
+              const SizedBox(height: 10,),
+            Expanded(
+              child: Container(
+                height: 100,
+                width: 400,
+                child: GridView.builder(
+                  shrinkWrap: true,
+                 // physics: const NeverScrollableScrollPhysics(),
+                  gridDelegate:const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3),
+                  itemCount: 9,
+                   itemBuilder:(context,index){
+                     return AnimatedBuilder(
+                      animation:Provider.of<GameProvider>(context),
+                      builder:(context,child){
+                    return GestureDetector(
+                      onTap: () =>gameProvider.makeMove(index),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.black),
+                        ),
+                        child: Center(
+                          child: Text(
+                           gameProvider.board[index],
+                            style: const TextStyle(fontSize: 40),
+                          ),
                         ),
                       ),
-                    ),
-                  );
-                 });},),
+                    );
+                   });},),
+              ),
             ),
-          ),
-           //  const SizedBox(height: 20,),
-           
-
-             IconButton(onPressed: (){
-              Provider.of(context,listen: false).resetGame();
-             },
-              icon:Icon(Icons.games_outlined,
-              size:20 ,))
-          
-        ],
-      ),
+             //  const SizedBox(height: 20,),
+             
+             ElevatedButton(onPressed:(){
+              gameProvider.resetGame();
+             }, 
+             child:Text('Reset'))
+      
+              //  IconButton(onPressed: (){
+              //   Provider.of(context).resetGame();
+              //  },
+              //   icon:Icon(Icons.games_outlined,
+              //   size:20 ,))
+            
+          ],
+        ),
+      );}
     )
     );
   }
--- lib/provider/game_provider.dart
+++ lib/provider/game_provider.dart
@@ -4,15 +4,19 @@ class GameProvider extends ChangeNotifier{
   List<String> _board= List.filled(9, '');
   String _currentPlayer='X';
   String _winner='';
+  bool _gameOver=false;
   List<String>get board=>_board;
   get currentplayer=>_currentPlayer;
   get winner => _winner;
+  get gameOver=> _gameOver;
 
     void makeMove(int index) {
-    if (_board[index] == '' && _winner == '') {
+    if (_board[index] == '' && !_gameOver) {
       _board[index] = _currentPlayer;
       _checkWinner();
-      _currentPlayer = _currentPlayer == 'X' ? 'O' : 'X';
+    if(!_gameOver){
+        _currentPlayer = _currentPlayer == 'X' ? 'O' : 'X';
+    }
       notifyListeners();
     }
   }
@@ -28,16 +32,23 @@ class GameProvider extends ChangeNotifier{
           _board[combination[0]] == _board[combination[1]] &&
           _board[combination[0]] == _board[combination[2]]) {
         _winner = _board[combination[0]];
+        
+        _gameOver=true;
+        notifyListeners();
         break;
     }
   }
-     if (_winner == '' && !_board.contains('')) {
+     if (!_board.contains('')) {
       _winner = 'Draw';
+      _gameOver=true;
+      notifyListeners();
     }
-     void resetGame() {
+    
+}
+ void resetGame() {
     _board = List.filled(9, '');
     _currentPlayer = 'X';
     _winner = '';
+    _gameOver=false;
     notifyListeners();
-  }
-}}
+  }}
